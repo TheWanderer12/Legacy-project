6f490e701714a6b3ddb81500c96fea8636af4fd7
Ravil <ravil.shainidze12@gmail.com>
2024-10-16 12:46:20 -0500

    export TreePage module to home

--- legacy-app/app/page.tsx
+++ legacy-app/app/page.tsx
@@ -1,12 +1,9 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import Tree from 'react-d3-tree';
+import TreePage from '@/app/tree/page';
 
 export default function Home() {
-  const [tree, setTree] = useState({
-    name: 'Root',
-    children: [],
-  });
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]" >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -25,7 +22,7 @@ export default function Home() {
         </ol>
       </main>
       <div>
-        <Tree data={tree} />
+        <TreePage />
       </div>
     </div >
   );

340fe8c1e4c83b6d8c4bfeda25393bd7dd512464
Ravil <ravil.shainidze12@gmail.com>
2024-10-17 20:43:28 -0500

    comment everything except first node to fix lint

--- legacy-app/app/tree/page.tsx
+++ legacy-app/app/tree/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
 import { Box } from "@chakra-ui/react";
 
@@ -8,28 +8,29 @@ const Tree = dynamic(() => import("react-d3-tree"), {
   ssr: false,
 });
 
-interface TreeNode {
-  name: string;
-  children: TreeNode[];
-}
+// interface TreeNode {
+//   name: string;
+//   children: TreeNode[];
+// }
 
 export default function TreePage() {
-  const [tree, setTree] = useState<TreeNode>({
-    name: "Root",
-    children: [],
-  });
+  // const [tree, setTree] = useState<TreeNode>({
+  //   name: "Root",
+  //   children: [],
+  // });
 
-  const addNode = () => {
-    const newNode: TreeNode = {
-      name: "Node",
-      children: [],
-    };
+  const tree = { name: "Root", children: [] };
+  // const addNode = () => {
+  //   const newNode: TreeNode = {
+  //     name: "Node",
+  //     children: [],
+  //   };
 
-    const newTree = { ...tree };
-    newTree.children.push(newNode);
-    setTree(newTree);
-  };
-  addNode();
+  //   const newTree = { ...tree };
+  //   newTree.children.push(newNode);
+  //   setTree(newTree);
+  // };
+
   return (
     <Box h="100vh" w="100vw">
       <Tree data={tree} />
